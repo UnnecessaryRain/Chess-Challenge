241d93b617a17837471d28b2259048b7200d346d
Jacob Gonzalez <jacobgonzalez5252@gmail.com>
2023-07-22 23:55:51 +1000

    add bonus tables

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,13 +7,99 @@ public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 1, 3, 3, 5, 9, 10 };
+    bool endgame = false;
+
+    // todo: encode into bit array of either 8 or 4 bit values
+    // we only need 2 ^ 7 for all values in this table. we can compress the array into a 8 64bit ints
+    // less if we use less resolution. e.g. 4 bits for 0 - 16 instead of -50 to 50
+    // this would mean 4 uint64 to store a pieces table, rather than 64 ints
+    int[,] bonuses = {
+        {
+            // king end game
+            -50,-40,-30,-20,-20,-30,-40,-50,
+            -30,-20,-10,  0,  0,-10,-20,-30,
+            -30,-10, 20, 30, 30, 20,-10,-30,
+            -30,-10, 30, 40, 40, 30,-10,-30,
+            -30,-10, 30, 40, 40, 30,-10,-30,
+            -30,-10, 20, 30, 30, 20,-10,-30,
+            -30,-30,  0,  0,  0,  0,-30,-30,
+            -50,-30,-30,-30,-30,-30,-30,-50
+        },
+        // pawn
+        {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            5,  5, 10, 25, 25, 10,  5,  5,
+            0,  0,  0, 20, 20,  0,  0,  0,
+            5, -5,-10,  0,  0,-10, -5,  5,
+            5, 10, 10,-20,-20, 10, 10,  5,
+            0,  0,  0,  0,  0,  0,  0,  0
+        },
+        // knight
+        {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  0,  0,  0,-20,-40,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50,
+        },
+        {
+            // bishop
+            -20,-10,-10,-10,-10,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5, 10, 10,  5,  0,-10,
+            -10,  5,  5, 10, 10,  5,  5,-10,
+            -10,  0, 10, 10, 10, 10,  0,-10,
+            -10, 10, 10, 10, 10, 10, 10,-10,
+            -10,  5,  0,  0,  0,  0,  5,-10,
+            -20,-10,-10,-10,-10,-10,-10,-20,
+        },
+        // rook
+        {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            5, 10, 10, 10, 10, 10, 10,  5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            0,  0,  0,  5,  5,  0,  0,  0
+        },
+        // queen
+        {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+            -5,  0,  5,  5,  5,  5,  0, -5,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+        },
+        // king
+        {
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -20,-30,-30,-40,-40,-30,-30,-20,
+            -10,-20,-20,-20,-20,-20,-20,-10,
+            20, 20,  0,  0,  0,  0, 20, 20,
+            20, 30, 10,  0,  0, 10, 30, 20
+        },
+
+    };
 
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
 
         Move? bestMove = null;
-        var bestScore = 0;
+        var bestScore = int.MinValue;
         foreach (var move in moves)
         {
             if (MoveIsCheckmate(board, move))
@@ -34,7 +120,7 @@ public class MyBot : IChessBot
         return bestMove ?? moves[0];
     }
 
-    public int scoreMove(Board board, Move move)
+    int scoreMove(Board board, Move move)
     {
         board.MakeMove(move);
 
@@ -42,22 +128,40 @@ public class MyBot : IChessBot
 
         var blackScore = 0;
         var whiteScore = 0;
+
+        var blackPieces = 0;
+        var whitePieces = 0;
+
         foreach (var pieceList in pieceLists)
         {
             foreach (var piece in pieceList)
             {
                 if (piece.IsWhite)
                 {
-                    whiteScore += 100 * pieceValue(piece) + rankSquare(piece.Square);
+                    whiteScore += 100 * pieceValue(piece) + rankPosition(piece);
+
+                    if (!piece.IsKing && !piece.IsPawn)
+                    {
+                        whitePieces += 1;
+                    }
                 }
                 else
                 {
-                    blackScore += 100 * pieceValue(piece) + rankSquare(piece.Square);
+                    blackScore += 100 * pieceValue(piece) + rankPosition(piece);
+                    if (!piece.IsKing && !piece.IsPawn)
+                    {
+                        blackPieces += 1;
+                    }
                 }
             }
         }
 
         board.UndoMove(move);
+
+        if (whitePieces <= 3 || blackPieces <= 3)
+        {
+            endgame = true;
+        }
 
         if (board.IsWhiteToMove)
         {
@@ -69,15 +173,16 @@ public class MyBot : IChessBot
         }
     }
 
-    // for now just uniformly rank the squares no matter what piece
-    public int rankSquare(Square s)
+    int rankPosition(Piece p)
     {
-        var middle = 64 / 2;
-        var dist = Math.Abs(s.Index - middle);
-        return 64 - dist;
+        if (p.IsKing && endgame)
+        {
+            return bonuses[0, p.Square.Index];
+        }
+        return bonuses[(int)p.PieceType, p.Square.Index];
     }
 
-    public int pieceValue(Piece p)
+    int pieceValue(Piece p)
     {
         return pieceValues[(int)p.PieceType];
     }
